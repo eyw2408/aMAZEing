e01732992817cd62976fe4840a6c400f24cb94ca
Jennifer Kim <Jennifer Kim>
2017-04-10 23:45:16 -0700

    Art and animation

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class MovementController : MonoBehaviour {
 	Transform tr;
 	Rigidbody2D rb;
+	Animator anim;
 	// Use this for initialization
 	void Start () {
 		tr = GetComponent<Transform> ();
 		rb = GetComponent<Rigidbody2D> ();
+		anim = this.gameObject.GetComponent<Animator>();
+
 	}
 	
 	// Update is called once per frame
@@ -16,8 +19,11 @@ public class MovementController : MonoBehaviour {
 		float vert = Input.GetAxis("Vertical");
 		float horizont = Input.GetAxis("Horizontal");
                 int SCALE = 4;
+
 		Vector2 vc2 = new Vector2 (horizont*SCALE,vert*SCALE);
+
 		// vc2.Normalize ();
 		rb.AddForce (vc2);
+		anim.SetFloat ("moving", Mathf.Abs(vert) + Mathf.Abs(horizont));
 	}
 }
