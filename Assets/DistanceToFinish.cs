494fe08c22fd8fdad1ce23191b634193761e4d21
jhorowitz16 <joshua.horowitz@berkeley.edu>
2017-04-30 01:40:35 -0700

    added a percentage-complete feature so the user knows how far he/she has gone

---
+++ Assets/DistanceToFinish.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DistanceToFinish : MonoBehaviour {
+
+    public Text distText;
+    public GameObject finishObj;
+
+    private GameObject player;
+    private float finishX;
+    private float finishY;
+    private float maxDist;
+
+    // Use this for initialization
+    void Start () {
+        finishX = finishObj.transform.position.x;
+        finishY = finishObj.transform.position.y;
+        player = GameObject.FindGameObjectWithTag("Player");
+        maxDist = calcDist();
+    }
+
+    float calcDist ()
+    {
+        float x = player.transform.position.x;
+        float y = player.transform.position.y;
+        float dist = ((x - finishX) * (x - finishX) + (y - finishY) * (y - finishY));
+        return dist;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        float dist = calcDist();
+        float percent = (maxDist - dist) / maxDist;
+        Debug.Log(percent);
+
+        distText.text = string.Format("{0:p0}", percent);
+
+    }
+}
---
+++ Assets/FlashBoulder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashBoulder : MonoBehaviour {
+
+    private TimerNew timer;
+    private GameObject[] boulders;
+	// Use this for initialization
+	void Start () {
+        timer = gameObject.GetComponent<TimerNew>();
+        boulders = GameObject.FindGameObjectsWithTag("Boulders");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int beatMod = timer.getBeatMod();
+        if (beatMod == 0) {
+            foreach (GameObject boulder in boulders)
+            {
+                boulder.SetActive(false);
+            }
+        }
+        else if (beatMod == 3)
+        {
+            foreach (GameObject boulder in boulders)
+            {
+                boulder.SetActive(true);
+            }
+        }
+    }
+}
